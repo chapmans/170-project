d43d1c2f10a797139ccab2d9cd907d1aea047042
Ivan Tham <itham@ucsd.edu>
2014-02-13 19:16:41 -0800

    show map and directions in the directions page and removed duplicate code

--- public/js/map.js
+++ public/js/map.js
@@ -2,40 +2,29 @@ function handleNoGeolocation() {
 	alert('Your browser does not support geolocation.')
 }
 
-function initMap() {
+function init() {
 	if(navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(setMap, handleNoGeolocation, {}); 
+		window.directionsDisplay = new google.maps.DirectionsRenderer();
+
+		if(window.setMap === true) {
+			window.map = new google.maps.Map(document.getElementById('map_panel'), {
+				zoom: 7,
+				content: "Location found using HTML5."
+			});
+			window.directionsDisplay.setMap(map);
+		}
+
+		if(window.setDirections === true) {
+			window.directionsDisplay.setPanel(document.getElementById('directions_panel'));
+		}
+
+		navigator.geolocation.getCurrentPosition(renderDisplay, handleNoGeolocation, {}); 
 	} else {
 		handleNoGeolocation();
 	}
 }
 
-function initDirections() {
-	if(navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(setDirections, handleNoGeolocation, {}); 
-	} else {
-		handleNoGeolocation();
-	}	
-}
-
-function setMap(pos) {
-	var map = new google.maps.Map(document.getElementById('panel'), {
-		zoom: 7,
-		content: "Location found using HTML5."
-	});
-
-	var directionsDisplay = new google.maps.DirectionsRenderer();
-	directionsDisplay.setMap(map);
-	renderDisplay(directionsDisplay, pos);
-}
-
-function setDirections(pos) {
-	var directionsDisplay = new google.maps.DirectionsRenderer();
-	directionsDisplay.setPanel(document.getElementById('panel'));
-	renderDisplay(directionsDisplay, pos);
-}
-
-function renderDisplay(directionsDisplay, pos) {
+function renderDisplay(pos) {
 	var currentLocation = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
 	var destination = "8657 Villa La Jolla Dr, La Jolla, CA 92037"; // TODO make it a param... how? since js script can't access {{}} params
 	var request = {
@@ -47,7 +36,10 @@ function renderDisplay(directionsDisplay, pos) {
 	var directionsService = new google.maps.DirectionsService();
 	directionsService.route(request, function(response, status) {
 		if (status == google.maps.DirectionsStatus.OK) {
-			directionsDisplay.setDirections(response);
+			window.directionsDisplay.setDirections(response);
 		}
 	});
+
+	window.setMap = false;
+	window.setDirections = false;
 }
