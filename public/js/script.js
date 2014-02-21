73bb06e460220a532fd001a00c2af0cd0cf794f9
Chapman Seto <caseto@ucsd.edu>
2014-02-20 21:12:45 -0800

    Restyle the app

--- public/js/script.js
+++ public/js/script.js
@@ -14,7 +14,12 @@ $(document).ready(function() {
 	// Load more
 	var category = $('.cat-name').text();
 	var num = 1;
-	var source   = '<li class="place"><div class="name"><a class="next" href="/places/{{category}}/{{index}}">GO</a><a class="open-down icon-uniF48B"></a><div class="title">{{place}}</div><div class="info">{{direction}} {{distance}} mi</div></div><div class="more-info"><div class="rating"><span class="icon-star"></span><span class="icon-star"></span><span class="icon-star"></span><span class="icon-star"></span><span class="icon-star-half"></span></div><div class="hours">Today\'s hours: {{hours.friday.[0]}} - {{hours.friday.[1]}}</div><div class="site"><a href="{{site}}">{{site}}</a></div><div class="payment">{{#if payment}}Accepts credit cards{{else}}Cash only{{/if}}</div></div></li>';
+	var source   = '<li class="place"><div class="name"><a class="next" href="/places/{{category}}/{{index}}">GO</a>' +
+									'<a class="open-down icon-uniF48B"></a><div class="title">{{place}}</div><div class="info">' +
+									'{{direction}} {{distance}} mi</div></div><div class="more-info"><div class="rating">{{rating}}</div>' +
+									'<div class="hours">Today\'s hours: {{hours.friday.[0]}} - {{hours.friday.[1]}}</div><div class="site">' +
+									'<a href="{{site}}">{{site}}</a></div>'+
+									'<div class="payment">{{#if payment}}Accepts credit cards{{else}}Cash only{{/if}}</div></div></li>';
 	//var source   = '<li class="place"><div class="name"><a class="next" href="/places/{{category}}/{{index}}">GO</a><a class="open-down icon-uniF48B"></a><div class="title">{{place}}</div><div class="info" id="info-{{uid}}"><script>setDistanceTo("{{address}}", "info-{{uid}}");</script></div></div><div class="more-info"><div class="rating"><span class="icon-star"></span><span class="icon-star"></span><span class="icon-star"></span><span class="icon-star"></span><span class="icon-star-half"></span></div><div class="hours">Today\'s hours: {{hours.friday.[0]}} - {{hours.friday.[1]}}</div><div class="site"><a href="{{site}}">{{site}}</a></div><div class="payment">{{#if payment}}Accepts credit cards{{else}}Cash only{{/if}}</div></div></li>';
 	var template = Handlebars.compile(source);
 
@@ -30,6 +35,18 @@ $(document).ready(function() {
 				var html = template(data[i]);
 				$('#places').append(html);
 				$('.place').last().on('click', placeClick);
+				$('.place').last().find('.rating').each(function() {
+					var rating = Number($(this).text());
+					var stars = "";
+					while (rating > 0.6) {
+						stars += '<span class="icon-star"></span>';
+						rating -= 1;
+					}
+					if (rating < 0.6 && rating > 0.4) {
+						stars += '<span class="icon-star-half"></span>';
+					}
+					$(this).html(stars);
+				});
 			}
 			if (!data[0].hasNext) {
 				$('.more').hide();
@@ -38,6 +55,18 @@ $(document).ready(function() {
 		});
 	});
 
+	$('.rating').each(function() {
+		var rating = Number($(this).text());
+		var stars = "";
+		while (rating > 0.6) {
+			stars += '<span class="icon-star"></span>';
+			rating -= 1;
+		}
+		if (rating < 0.6 && rating > 0.4) {
+			stars += '<span class="icon-star-half"></span>';
+		}
+		$(this).html(stars);
+	});
 });
 
 function placeClick(e) {
